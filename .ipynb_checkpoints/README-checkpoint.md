b8b189598266c706acf3fe79e3cf15aefc64a34c
Vaxier8 <xmb1@pitt.edu>
2022-10-05 12:27:09 -0400

    Added the Python File

---
+++ .ipynb_checkpoints/README-checkpoint.md
@@ -0,0 +1,2 @@
+# 0010-skills-lab-vc
+Group Members: Xavier Bear, Justin Bailey
---
+++ .ipynb_checkpoints/ReturnName-checkpoint.py
@@ -0,0 +1,7 @@
+def ReturnName():
+    name = input(print("What is your Name?"))
+    age = input(priny("What is your Age"))
+    
+    print("Your name is : " + name + "Your age is : " + str(age))
+
+ReturnName()
